badb4b55ad4adc0b31394b89712f3b515ab2c012
kae-blog <jing180804@gmail.com>
2025-11-14 01:31:54 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,17 @@
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
-export const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    publishDate: z.string(), // 改成 string，原本是 z.date()
-    previewImage: z.string().optional(),
-    heroImage: z.string().optional(),
-    category: z.array(z.string()).optional(),
-    description: z.string().optional(),
-  }),
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date(), // 我們維持它是 date 型別
+    category: z.array(z.string()),
+    previewImage: z.string(),
+    heroImage: z.string().optional(), // 這是你新增的欄位，設為選填
+    tags: z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = {
-  blog: blogCollection,
+  'blog': blogCollection,
 };
