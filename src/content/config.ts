6f5dcdc8fc01155ca275b28296d7838dabb0d9aa
kae-blog <jing180804@gmail.com>
2025-11-13 23:42:04 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
     publishDate: z.date(),
-    category: z.string(), // 這是你文章的「分類」
+    category: z.array(z.string()), // <-- 唯一的修改點在這裡
     previewImage: z.string(),
     tags: z.array(z.string()).optional(),
   }),
 });
+
+export const collections = {
+  'blog': blogCollection,
+};
