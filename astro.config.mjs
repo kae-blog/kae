504b7adef0c6b70202b1849063e36ea1662a1fbf
kae-blog <jing180804@gmail.com>
2025-11-14 23:59:18 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { fileURLToPath } from 'url'; // 👈 1. 加入這行
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // ...您其他的 Astro 設定，例如 site, base 等...
+
+  // 👇 2. 加入這個 vite 區塊
+  vite: {
+    resolve: {
+      alias: {
+        '@src': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  }
 });
